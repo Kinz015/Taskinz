673627f25a61d58d622a3d6788f687603ae56c83
Thomas <guilhermelins978@gmail.com>
2025-12-19 13:58:49 -0300

    Criei o menu de ações

--- src/componentes/tasks/BodyTasksTable/index.tsx
+++ src/componentes/tasks/BodyTasksTable/index.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { TaskDTO } from "@/types/task";
 import { StatusBadge } from "../StatusBadge";
 import { EllipsisIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 type BodyTasksTableProps = {
   tasks: TaskDTO[];
 };
 
 export default function BodyTasksTable({ tasks }: BodyTasksTableProps) {
+  const [openMenuId, setOpenMenuId] = useState<number | null>(null);
+
   return (
     <table className="px-2 w-full border-separate border-spacing-y-2">
       <colgroup>
@@ -46,13 +51,54 @@ export default function BodyTasksTable({ tasks }: BodyTasksTableProps) {
               {new Date(task.updatedAt).toLocaleDateString()}
             </td>
 
-            <td className="rounded-r-lg">
-              <Link
+            <td className="relative rounded-r-lg overflow-visible">
+              <button
+                onClick={() =>
+                  setOpenMenuId(openMenuId === task.id ? null : task.id)
+                }
+                className={`w-full h-full flex justify-center py-4 hover:bg-gray-300 
+                  hover:cursor-pointer rounded-r-lg
+                  ${openMenuId === task.id ? "py-7" : ""}`}
+              >
+                <EllipsisIcon />
+              </button>
+
+              {openMenuId === task.id && (
+                <div className="absolute right-0 top-full mt-2 z-50 w-40 bg-white rounded-md shadow-lg border">
+                  <Link
+                    href={`/tasks/${task.id}`}
+                    className="block px-4 py-2 text-sm hover:bg-gray-100 hover:rounded-md"
+                  >
+                    Abrir tarefa
+                  </Link>
+
+                  <button
+                    onClick={() => {
+                      console.log("Editar", task.id);
+                      setOpenMenuId(null);
+                    }}
+                    className="w-full text-left px-4 py-2 text-sm hover:bg-gray-100 hover:cursor-pointer"
+                  >
+                    Editar tarefa
+                  </button>
+                  <button
+                    onClick={() => {
+                      console.log("Editar", task.id);
+                      setOpenMenuId(null);
+                    }}
+                    className="w-full text-left px-4 py-2 text-sm hover:bg-gray-100 hover:rounded-md hover:cursor-pointer"
+                  >
+                    Fechar
+                  </button>
+                </div>
+              )}
+
+              {/* <Link
                 href={`/tasks/${task.id}`}
                 className="w-full h-full flex justify-center py-4 hover:bg-gray-300 hover:cursor-pointer rounded-r-lg"
               >
                 <EllipsisIcon />
-              </Link>
+              </Link> */}
             </td>
           </tr>
         ))}
